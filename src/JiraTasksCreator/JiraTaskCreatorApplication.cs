f629cfb2ef54d59948749da3d7286adff8691a9d
Marcin Salata <marcin.salata@hotmail.com>
2022-08-15 14:56:49 +0200

    add serilog

--- src/JiraTasksCreator/JiraTaskCreatorApplication.cs
+++ src/JiraTasksCreator/JiraTaskCreatorApplication.cs
@@ -1,23 +1,30 @@
+using Microsoft.Extensions.Logging;
+
 public class JiraTaskCreatorApplication
 {
     private readonly IJiraTaskCreator _jiraTaskCreator;
     private readonly ApplicationSettings _settings;
+    private readonly ILogger<JiraTaskCreatorApplication> _logger;
 
-    public JiraTaskCreatorApplication(IJiraTaskCreator jiraTaskCreator, ApplicationSettings settings)
+    public JiraTaskCreatorApplication(IJiraTaskCreator jiraTaskCreator, ApplicationSettings settings, ILogger<JiraTaskCreatorApplication> logger)
     {
         _jiraTaskCreator = jiraTaskCreator;
         _settings = settings;
+        _logger = logger;
     }
 
     public async Task RunAsync(CancellationToken ct)
     {
+        _logger.LogInformation($"Reading links from {_settings.LinksFilePath}");
         var lines = await System.IO.File.ReadAllLinesAsync(_settings.LinksFilePath, ct);
 
         foreach (var line in lines)
         {
+            _logger.LogInformation($"Creating jira task for {line}");
             await _jiraTaskCreator.CreateTaskAsync(line, ct);
         }
 
+        _logger.LogInformation($"Deleting links from {_settings.LinksFilePath}");
         await System.IO.File.WriteAllLinesAsync(_settings.LinksFilePath, new List<string>(0), ct);
     }
 }
--- src/JiraTasksCreator/Program.cs
+++ src/JiraTasksCreator/Program.cs
@@ -2,46 +2,60 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
-var config = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .AddEnvironmentVariables()
-                    .Build();
-var settings = config.GetRequiredSection("Settings").Get<ApplicationSettings>();
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(".//logs//log.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7)
+    .CreateLogger();
 
-var builder = new HostBuilder()
-               .ConfigureServices((hostContext, services) =>
-               {
-                   services.AddHttpClient("Jira", c =>
-                   {
-                       c.BaseAddress = new Uri(settings.Jira.Url);
-                       c.DefaultRequestHeaders.Authorization =
-                           new AuthenticationHeaderValue("Basic", HttpUtiltyService.CreateBasicAuthValue(settings.Jira.User, settings.Jira.Token));
-                   });
-                   services.AddScoped<IJiraTaskCreator, JiraTaskCreator>();
-                   services.AddScoped<IHttpHandler, HttpHandler>();
-                   services.AddScoped<IWebpageRetriever, WebpageRetriever>();
-                   services.AddSingleton<ApplicationSettings>(settings);
-                   services.AddSingleton<JiraConfiguration>(settings.Jira);
-                   services.AddSingleton<JiraTaskCreatorApplication>();
-               }).UseConsoleLifetime();
-
-var host = builder.Build();
-
-using (var serviceScope = host.Services.CreateScope())
+try
 {
-    var services = serviceScope.ServiceProvider;
+    Log.Logger.Information("Starting application");
+    var config = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json")
+                        .AddEnvironmentVariables()
+                        .Build();
+    var settings = config.GetRequiredSection("Settings").Get<ApplicationSettings>();
 
-    try
+    var builder = new HostBuilder()
+                   .ConfigureServices((hostContext, services) =>
+                   {
+                       services.AddHttpClient("Jira", c =>
+                       {
+                           c.BaseAddress = new Uri(settings.Jira.Url);
+                           c.DefaultRequestHeaders.Authorization =
+                               new AuthenticationHeaderValue("Basic", HttpUtiltyService.CreateBasicAuthValue(settings.Jira.User, settings.Jira.Token));
+                       });
+                       services.AddScoped<IJiraTaskCreator, JiraTaskCreator>();
+                       services.AddScoped<IHttpHandler, HttpHandler>();
+                       services.AddScoped<IWebpageRetriever, WebpageRetriever>();
+                       services.AddSingleton<ApplicationSettings>(settings);
+                       services.AddSingleton<JiraConfiguration>(settings.Jira);
+                       services.AddSingleton<JiraTaskCreatorApplication>();
+                   })
+                   .UseSerilog()
+                   .UseConsoleLifetime();
+
+    var host = builder.Build();
+
+    using (var serviceScope = host.Services.CreateScope())
     {
+        var services = serviceScope.ServiceProvider;
         var cts = new CancellationTokenSource(settings.Timeout * 1000 * 60);
         var myService = services.GetRequiredService<JiraTaskCreatorApplication>();
         await myService.RunAsync(cts.Token);
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine("Error Occured");
-    }
+
+    Log.Logger.Information("Closing application");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
 return 0;
